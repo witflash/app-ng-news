335a1d0160ba6e2bd31b2e8a8eb9a736d1872d9a
witflash <witflash@gmail.com>
2018-06-16 11:56:06 +0300

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule, Http } from "@angular/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { ApiService } from './api.service';
 import { NewsListComponent } from './news-list/news-list.component';
 import { NewsItemComponent } from './news-item/news-item.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, NewsListComponent, NewsItemComponent],
-  imports: [BrowserModule, HttpModule],
+  declarations: [AppComponent, NewsListComponent, NewsItemComponent, SearchComponent],
+  imports: [BrowserModule, HttpModule, FormsModule],
   providers: [ApiService],
   bootstrap: [AppComponent],
 })
